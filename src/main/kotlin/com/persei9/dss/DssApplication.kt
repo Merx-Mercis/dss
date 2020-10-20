33487f2e11d240acb4fdb62eadfa4324370e5f37
Felipe Bermudez <felipeberm@gmail.com>
2020-10-20 14:41:20 -0500

    initial commit

---
+++ src/main/kotlin/com/persei9/dss/DssApplication.kt
@@ -0,0 +1,11 @@
+package com.persei9.dss
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DssApplication
+
+fun main(args: Array<String>) {
+	runApplication<DssApplication>(*args)
+}
---
+++ src/test/kotlin/com/persei9/dss/DssApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.persei9.dss
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DssApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
